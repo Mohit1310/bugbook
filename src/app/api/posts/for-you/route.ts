a569ee2618748c3f89983892c4c9aeb935c59eb3
Mohit Dayma <mohitdayama1234@gmail.com>
2024-07-28 15:19:29 +0530

    adding infinite scrolling

--- src/app/api/posts/for-you/route.ts
+++ src/app/api/posts/for-you/route.ts
@@ -1,14 +1,19 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
+import { PostsPage, postDataInclude } from "@/lib/types";
+import { NextRequest } from "next/server";
 
 /**
  * Handles a GET request by validating the request, checking user authentication,
  * and returning a list of posts in descending order of creation date.
  * @returns {Promise} A Promise that resolves with the list of posts or an error response.
  */
-export async function GET() {
+export async function GET(req: NextRequest) {
   try {
+    const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
+
+    const pageSize = 10;
+
     const { user } = await validateRequest();
 
     if (!user) {
@@ -18,9 +23,18 @@ export async function GET() {
     const posts = await prisma.post.findMany({
       include: postDataInclude,
       orderBy: { createdAt: "desc" },
+      take: pageSize + 1,
+      cursor: cursor ? { id: cursor } : undefined,
     });
 
-    return Response.json(posts);
+    const nextCursor = posts.length > pageSize ? posts[pageSize].id : null;
+
+    const data: PostsPage = {
+      posts: posts.slice(0, pageSize),
+      nextCursor,
+    };
+
+    return Response.json(data);
   } catch (error) {
     console.error(error);
     return Response.json({ error: "Internal Server Error" }, { status: 500 });
