34f373e64806cf8decebb8f77e98d76df294000c
Mohit Dayma <mohitdayama1234@gmail.com>
2024-07-31 14:57:14 +0530

    user tooltip and linkify

---
+++ src/components/Linkify.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import { LinkIt, LinkItUrl } from "react-linkify-it";
+import UserLinkWithTooltip from "./UserLinkWithTooltip";
+
+interface LinkifyProps {
+  children: React.ReactNode;
+}
+
+export default function Linkify({ children }: LinkifyProps) {
+  return (
+    <LinkifyUsername>
+      <LinkifyHastag>
+        <LinkifyUrl>{children}</LinkifyUrl>
+      </LinkifyHastag>
+    </LinkifyUsername>
+  );
+}
+
+function LinkifyUrl({ children }: LinkifyProps) {
+  return (
+    <LinkItUrl className="text-primary hover:underline">{children}</LinkItUrl>
+  );
+}
+
+function LinkifyUsername({ children }: LinkifyProps) {
+  return (
+    <LinkIt
+      regex={/(@[a-zA-Z0-9_-]+)/}
+      component={(match, key) => (
+        <UserLinkWithTooltip key={key} username={match.slice(1)}>
+          {match}
+        </UserLinkWithTooltip>
+      )}
+    >
+      {children}
+    </LinkIt>
+  );
+}
+
+function LinkifyHastag({ children }: LinkifyProps) {
+  return (
+    <LinkIt
+      regex={/#[a-zA-Z0-9]+/}
+      component={(match, key) => (
+        <Link
+          key={key}
+          href={`/hashtag/${match.slice(1)}`}
+          className="text-primary hover:underline"
+        >
+          {match}
+        </Link>
+      )}
+    >
+      {children}
+    </LinkIt>
+  );
+}
