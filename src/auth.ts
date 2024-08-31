c494e34a118113884100af1e257a395945d3913a
Mohit Dayma <mohitdayama1234@gmail.com>
2024-08-31 15:49:06 +0530

    feat: search + rewrites

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,7 @@ import prisma from "./lib/prisma";
 import { Lucia, Session, User } from "lucia";
 import { cache } from "react";
 import { cookies } from "next/headers";
+import { Google } from "arctic";
 
 /**
  * Creates a new PrismaAdapter instance with the provided session and user objects from Prisma.
@@ -59,6 +60,12 @@ interface DatabaseUserAttributes {
   avatarUrl: string | null;
   googleId: string | null;
 }
+
+export const google = new Google(
+  process.env.GOOGLE_CLIENT_ID!,
+  process.env.GOOGLE_CLIENT_SECRET!,
+  `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/callback/google`,
+);
 
 export const validateRequest = cache(
   async (): Promise<
